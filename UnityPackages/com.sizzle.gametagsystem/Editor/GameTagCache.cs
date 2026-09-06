ee80ba193a234684b250b55c4b4ba56b55eaff79
siz20971 <siz20971@gmail.com>
2026-09-06 16:46:22 +0900

    최적화: 에디터 GameTagCache 검색을 HashSet 기반으로 전환하여 중복 검사 성능 개선

--- UnityPackages/com.sizzle.gametagsystem/Editor/GameTagCache.cs
+++ UnityPackages/com.sizzle.gametagsystem/Editor/GameTagCache.cs
@@ -4,42 +4,56 @@ namespace Sizzle.GameTagSystem.Editor
 {
     public static class GameTagCache
     {
-        private static List<GameTag> m_cachedGameTags = new List<GameTag>();
+        private static HashSet<GameTag> m_cachedGameTagSet = new HashSet<GameTag>();
+        private static List<GameTag> m_cachedGameTagList = new List<GameTag>();
+        private static bool m_isDirty = false;
 
         public static IList<GameTag> GetCachedGameTags()
         {
-            return m_cachedGameTags.AsReadOnly();
+            if (m_isDirty)
+                RebuildList();
+
+            return m_cachedGameTagList.AsReadOnly();
         }
         
         public static bool Contains(GameTag tag)
         {
-            return m_cachedGameTags.Contains(tag);
+            return m_cachedGameTagSet.Contains(tag);
         }
 
         public static void Add(GameTag tag)
         {
-            if (!m_cachedGameTags.Contains(tag))
-                m_cachedGameTags.Add(tag);
+            if (m_cachedGameTagSet.Add(tag))
+                m_isDirty = true;
         }
         
         public static void AddRange(IEnumerable<GameTag> tags)
         {
             foreach (GameTag tag in tags)
             {
-                if (!m_cachedGameTags.Contains(tag))
-                    m_cachedGameTags.Add(tag);
+                if (m_cachedGameTagSet.Add(tag))
+                    m_isDirty = true;
             }
         }
         
         public static void Remove(GameTag tag)
         {
-            if (m_cachedGameTags.Contains(tag))
-                m_cachedGameTags.Remove(tag);
+            if (m_cachedGameTagSet.Remove(tag))
+                m_isDirty = true;
         }
         
         public static void ClearCache()
         {
-            m_cachedGameTags.Clear();
+            m_cachedGameTagSet.Clear();
+            m_cachedGameTagList.Clear();
+            m_isDirty = false;
+        }
+
+        private static void RebuildList()
+        {
+            m_cachedGameTagList.Clear();
+            m_cachedGameTagList.AddRange(m_cachedGameTagSet);
+            m_isDirty = false;
         }
     }
 }
